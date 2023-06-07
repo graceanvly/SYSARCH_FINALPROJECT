62ec9891cefed750fcc7edff326ac2fa1c397dce
Grace An Velayo <graceann151@gmail.com>
2023-06-08 00:23:02 +0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <NavBar />
+        <div className='pages'>
+          <Routes>
+            <Route 
+              path = "/"
+              element = {<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
